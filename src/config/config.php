af72c13074b3ae2a093d53c447f5002849a1f3af
taqmaninw <taqmaninw@gmail.com>
2013-06-17 07:45:29 +0700

    Update config.php

--- src/config/config.php
+++ src/config/config.php
@@ -6,9 +6,9 @@ return array(
      *  */
     'Oauth' => array(
         'Google' => array(
-            'clientID' => '474246563221.apps.googleusercontent.com',
-            'clientSeceret' => 'xN8YUZQqExbJPXebjiaR3tMO',
-            'redirectUrl' => 'https://mycms.com/oauth2callback',
+            'clientID' => '',
+            'clientSeceret' => '',
+            'redirectUrl' => '',
             'ApiKey' => ''
         ),
         'Facebook'=> array(
