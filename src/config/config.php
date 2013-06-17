84ecce504c1cc29f05607b069abe392cd52c1288
krissanawat <krissanawat@flexmedia.co.th>
2013-06-17 07:45:02 +0700

    split commit

---
+++ src/config/config.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+    /*
+     * App credential get it form Google Console
+     *  */
+    'Oauth' => array(
+        'Google' => array(
+            'clientID' => '474246563221.apps.googleusercontent.com',
+            'clientSeceret' => 'xN8YUZQqExbJPXebjiaR3tMO',
+            'redirectUrl' => 'https://mycms.com/oauth2callback',
+            'ApiKey' => ''
+        ),
+        'Facebook'=> array(
+          'appkey'=>'',
+          'appsecret'=>'',
+          'redirecturl'=>''
+        ),
+        'Twitter'=>array(
+            
+        )
+    )
+);
+?>
